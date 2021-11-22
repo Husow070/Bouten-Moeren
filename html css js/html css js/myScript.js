eabb55c386b33fb45c09b07fd43b28d85fa24329
Husow070 <71512853+Husow070@users.noreply.github.com>
2021-11-22 11:56:37 +0100

    Add files via upload

--- html css js/html css js/myScript.js
+++ html css js/html css js/myScript.js
@@ -1,6 +1,8 @@
 let modal = document.querySelector("myModal")
 let btn = document.querySelector(".buttoncolor");
+let xclose = document.querySelector(".close");
 
+console.log(xclose);
 
 
 function showPupUp(){
@@ -11,3 +13,11 @@ function showPupUp(){
 
 btn.addEventListener("click", showPupUp);
 
+function closeWindow(){
+  console.log('1213')
+  const pop = document.querySelector(".modal-content");
+  pop.classList.add("hide");
+
+}
+
+xclose.addEventListener("click", closeWindow);
